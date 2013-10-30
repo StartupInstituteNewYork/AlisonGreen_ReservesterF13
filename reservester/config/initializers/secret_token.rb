1d9eed88fe3b1b7e74f67af6593e9ceb03ed1f8f
aligreen <aligreen@ccs.neu.edu>
2013-10-30 14:53:37 -0400

    reservester inital commit

---
+++ reservester/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reservester::Application.config.secret_token = '9f11f74db7ad878b90f10561e9f4b4c3ba7c7c1c0383a9945265bb3b39d5dc4fc13678ea80e22d63d19f9463cb75bce197790a1d7c07add64ef5bb6e001d4fad'
