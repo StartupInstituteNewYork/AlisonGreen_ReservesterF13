666bcbfc133dbe417985f7e5ad42f1fc1d305f37
aligreen <aligreen@ccs.neu.edu>
2013-11-11 13:07:25 -0500

    adding carrierwave/imagemagick/uploading images

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :address, :description, :phone, :title
-
+  attr_accessible :address, :description, :phone, :title, :image
   validates :title, presence: true,
   					length: { minimum: 3 }
+  mount_uploader :image, ImageUploader
 end
